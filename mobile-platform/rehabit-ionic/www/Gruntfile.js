1f040f8400aa569f4f1d93536f805e0b6163deea
Julia Nocera <giulz42@gmail.com>
2015-10-12 18:09:55 -0700

    added mobile platform with ionic

---
+++ mobile-platform/rehabit-ionic/www/Gruntfile.js
@@ -0,0 +1,147 @@
+module.exports = function(grunt) {
+
+  grunt.initConfig({
+    pkg: grunt.file.readJSON('../package.json'),
+    concat: {
+      dist: {
+        src: ['client/app/**/*.js'],
+        dest: 'client/dist/<%= pkg.name %>.js'
+      }
+    },
+
+    mochaTest: {
+      test: {
+        options: {
+          reporter: 'spec'
+        },
+        src: ['test/*.js']
+      }
+    },
+
+    karma: {
+      unit: {
+        configFile: 'karma.conf.js'
+      }
+    },
+
+    nodemon: {
+      dev: {
+        script: 'index.js'
+      }
+    },
+
+    uglify: {
+      options: {
+        banner: '/*! <%= pkg.name %> <%= grunt.template.today("dd-mm-yyyy") %> */\n'
+      },
+      dist: {
+        files: {
+          'client/dist/<%= pkg.name %>.min.js': ['<%= concat.dist.dest %>']
+        }
+      }
+    },
+
+    jshint: {
+      files: [
+        'index.js',
+        'client/**/*.js',
+        'server/**/*.js'
+      ],
+      options: {
+        force: false,
+        jshintrc: '.jshintrc',
+        ignores: [
+          'client/lib/**/*.js',
+          'client/dist/**/*.js'
+        ]
+      }
+    },
+
+    cssmin: {
+      dist: {
+        files: {
+          'client/dist/style.min.css': 'client/styles/style.css'
+        }
+      }
+    },
+
+    watch: {
+      scripts: {
+        files: [
+          'client/app/**/*.js',
+          'server/**/*.js'
+        ],
+        tasks: [
+          'concat',
+          'uglify'
+        ]
+      },
+      css: {
+        files: 'client/styles/*.css',
+        tasks: ['cssmin']
+      }
+    },
+
+    shell: {
+      prodServer: {
+        command: [
+          'git push heroku staging:master'
+        ].join('&&')
+      }
+    },
+  });
+
+  grunt.loadNpmTasks('grunt-contrib-uglify');
+  grunt.loadNpmTasks('grunt-contrib-jshint');
+  grunt.loadNpmTasks('grunt-contrib-watch');
+  grunt.loadNpmTasks('grunt-contrib-concat');
+  grunt.loadNpmTasks('grunt-contrib-cssmin');
+  grunt.loadNpmTasks('grunt-mocha-test');
+  grunt.loadNpmTasks('grunt-karma');
+  grunt.loadNpmTasks('grunt-shell');
+  grunt.loadNpmTasks('grunt-nodemon');
+
+  grunt.registerTask('server-dev', function (target) {
+    // Running nodejs in a different process and displaying output on the main console
+    var nodemon = grunt.util.spawn({
+         cmd: 'grunt',
+         grunt: true,
+         args: 'nodemon'
+    });
+    nodemon.stdout.pipe(process.stdout);
+    nodemon.stderr.pipe(process.stderr);
+
+    grunt.task.run([ 'watch' ]);
+  });
+
+  ////////////////////////////////////////////////////
+  // Main grunt tasks
+  ////////////////////////////////////////////////////
+
+  grunt.registerTask('test', [
+    'jshint',
+    'mochaTest',
+    'karma'
+  ]);
+
+  grunt.registerTask('build', [
+    'concat',
+    'uglify',
+    'cssmin'
+  ]);
+
+  grunt.registerTask('upload', function(n) {
+    if (grunt.option('prod')) {
+      grunt.task.run([ 'shell' ]);
+    } else {
+      grunt.task.run([ 'server-dev' ]);
+    }
+  });
+
+   grunt.registerTask('deploy', [
+    'test',
+    'build',
+    'upload'
+  ]);
+
+};
---
+++ mobile-platform/rehabit-ionic/www/js/app.js
@@ -0,0 +1,101 @@
+angular.module('app', [
+  'app.directives',
+  'app.services',
+  'app.create',
+  'app.edit',
+  'app.dashboard',
+  'app.auth',
+  'ngRoute',
+  'ngSanitize',
+  'gridshore.c3js.chart',
+  'satellizer',
+  'cgNotify'
+])
+
+.config(['$routeProvider', '$httpProvider', '$authProvider',
+  function ($routeProvider, $httpProvider, $authProvider) {
+    $routeProvider
+      .when('/signin', {
+        templateUrl: 'js/auth/signin.html',
+        controller: 'AuthController',
+      })
+      .when('/signup', {
+        templateUrl: 'js/auth/signup.html',
+        controller: 'AuthController',
+      })
+      .when('/signout', {
+        template: '',
+        controller: 'AuthController',
+      })
+      .when('/dashboard', {
+        templateUrl: 'js/dashboard/dashboard.html',
+        controller: 'DashboardController',
+        // authenticate: true
+      })
+      .when('/create', {
+        templateUrl: 'js/create/create.html',
+        controller: 'CreateController',
+        // authenticate: true
+      })
+      .when('/edit', {
+        templateUrl: 'js/edit/edit.html',
+        controller: 'EditController',
+        // authenticate: true
+      })
+      .otherwise({
+        redirectTo: '/dashboard'
+      });
+
+    $authProvider.loginUrl = '/signin';
+    $authProvider.signupUrl = '/signup';
+    $authProvider.tokenPrefix = 'habit';
+
+    $authProvider.google({
+      clientId: '416143587162-phs72qq27pfvqua6buqb5lf4okum9krq.apps.googleusercontent.com',
+      url: 'http://localhost:8080/authenticate/google'
+    });
+
+    $httpProvider.interceptors.push('AttachTokens');
+  }
+])
+
+.factory('AttachTokens', ['$window',
+  function ($window) {
+    var attach = {
+      request: function (object) {
+        var jwt = $window.localStorage.getItem('habit_token');
+        if (jwt) {
+          object.headers.Authorization = 'Bearer ' + jwt;
+        }
+        object.headers['Allow-Control-Allow-Origin'] = '*';
+        return object;
+      }
+    };
+    return attach;
+  }
+])
+
+.run(['$rootScope', '$location', '$interval', 'Auth', 'Events', 'Habits',
+  function ($rootScope, $location, $interval, Auth, Events, Habits) {
+    $rootScope.$on('$routeChangeStart', function (event, next, current) {
+      if (next.$$route && next.$$route.authenticate && !Auth.isAuth()) {
+        $location.path('/signin');
+      }
+    });
+    var timer;
+    var eventScheduler = function() {
+      console.log('eventScheduler start');
+      Habits.getHabits()
+        .then(function (habits) {
+          events = Events.getEventQueue(habits);
+          timer = $interval(function() {
+            if (events.length) {
+              Events.triggerEvents(events);
+            }
+          }, 1000);
+        });
+    };
+    eventScheduler();
+    $rootScope.$on('habitChange', eventScheduler);
+  }
+]);
